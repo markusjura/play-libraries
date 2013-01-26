856909327fd4d99c511edf8abcdb08591e539f1f
Markus Jura <markus.jura@googlemail.com>
2013-01-26 11:17:59 -0800

    changed dependency to own repo
    Signed-off-by: Markus Jura <markus.jura@googlemail.com>

--- apis/s3/project/Build.scala
+++ apis/s3/project/Build.scala
@@ -7,18 +7,10 @@ object ApplicationBuild extends Build {
   val appName = "api-s3"
   val appVersion = "1.6.1"
 
-  val appDependencies = Seq(
-    "nl.rhinofly" %% "api-aws-utils" % "1.3.1")
-
-  def rhinoflyRepo(version: String) = {
-    val repo = if (version endsWith "SNAPSHOT") "snapshot" else "release"
-    Some("Rhinofly Internal " + repo.capitalize + " Repository" at "http://maven-repository.rhinofly.net:8081/artifactory/libs-" + repo + "-local")
-  }
+  val appDependencies = Seq("nl.rhinofly" %% "api-aws-utils" % "1.3.1")
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
     organization := "nl.rhinofly",
-    resolvers += rhinoflyRepo("RELEASE").get,
-    publishTo <<= version(rhinoflyRepo),
-    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"))
+    resolvers += Resolver.url("Markus Jura Play Libraries GitHub Repository", url("http://markusjura.github.com/play-libraries/releases/"))(Resolver.ivyStylePatterns))
 
 }
